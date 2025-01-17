5daa06912be59bc05904cf1d4ac1fb57fd206d33
Chalo Salvador <chalo@monogram.io>
2025-01-17 12:15:17 +0100

    first commit

---
+++ idx-template.nix
@@ -0,0 +1,22 @@
+{ pkgs, packageManager ? "npm", ... }: {
+  packages = [
+    pkgs.nodejs_20
+    pkgs.yarn
+    pkgs.jq
+  ];
+  bootstrap = ''
+    mkdir -p "$WS_NAME"
+    ${if packageManager == "yarn" 
+      then "yarn create react-native-app \"$WS_NAME\" --template react-native-template-typescript" 
+      else "npx react-native init \"$WS_NAME\" --template react-native-template-typescript"
+    }
+    
+    mkdir "$WS_NAME/.idx/"
+    packageManager=${packageManager} j2 ${./devNix.j2} -o "$WS_NAME/.idx/dev.nix"
+    nixfmt "$WS_NAME/.idx/dev.nix"
+    packageManager=${packageManager} j2 ${./README.j2} -o "$WS_NAME/README.md"
+    
+    chmod -R +w "$WS_NAME"
+    mv "$WS_NAME" "$out"
+  '';
+}
