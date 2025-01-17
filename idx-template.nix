949f0c8368c538af2ed6d915d9a0abb16a01cb0d
Chalo Salvador <chalo@monogram.io>
2025-01-17 13:25:56 +0100

    Update

--- idx-template.nix
+++ idx-template.nix
@@ -7,8 +7,8 @@
   bootstrap = ''
     mkdir -p "$WS_NAME"
     ${if packageManager == "yarn" 
-      then "yarn create react-native-app \"$WS_NAME\" --template typescript" 
-      else "npx react-native@latest init \"$WS_NAME\" --template react-native"
+      then "yarn dlx @react-native-community/cli init \"$WS_NAME\" --template react-native-template-typescript" 
+      else "npx @react-native-community/cli init \"$WS_NAME\" --template react-native-template-typescript"
     }
     
     mkdir "$WS_NAME/.idx/"
